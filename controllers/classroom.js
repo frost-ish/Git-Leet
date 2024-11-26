a95d65123f08e6f492e53c697136a492050ac9df
Ishita <iishita_be22@thapar.edu>
2024-11-27 04:34:24 +0530

    minor fix

--- controllers/classroom.js
+++ controllers/classroom.js
@@ -27,13 +27,12 @@ const getClassroomById = async (req, res) => {
 	const { id } = req.params;
 	try {
 		const classroom = await Classroom.findByPk(id);
-	
+
 		res.status(200).json({
 			classroom,
 		});
-	}
-	catch (error){
-		res.status(400).json({ message: error});
+	} catch (error) {
+		res.status(400).json({ message: error });
 		return;
 	}
 };
@@ -66,11 +65,11 @@ const joinClassroom = async (req, res) => {
 const getClassroomsForStudent = async (req, res) => {
 	const user = await User.findByPk(req.user.email);
 	const student = await user.getStudent();
-	try{
-		const classrooms = await student.getClassrooms();
-	}
-	catch (error){
-		res.status(400).json({ message: error});
+	var classrooms;
+	try {
+		classrooms = await student.getClassrooms();
+	} catch (error) {
+		res.status(400).json({ message: error });
 		return;
 	}
 
